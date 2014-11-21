f728bf768704b393bd71adcb076fcd1f16cb6eb0
sodapopinsky <Nick@Nicholass-iMac.local>
2014-11-21 10:39:09 -0600

    initial

---
+++ www/js/factories/InboxFactory.js
@@ -0,0 +1,37 @@
+/**
+ * Factory: InboxFactory
+ */
+angular.module('starter')
+  .factory('InboxFactory', function InboxFactory ($q, $http, $location) {
+    'use strict';
+    var exports = {};
+
+    exports.messages = [];
+
+    exports.goToMessage = function(id) {
+      if ( angular.isNumber(id) ) {
+        console.log('inbox/email/' + id)
+        $location.path('inbox/email/' + id)
+      }
+    }
+
+    exports.deleteMessage = function (id, index) {
+      this.messages.splice(index, 1);
+    }
+
+    exports.getMessages = function () {
+      var deferred = $q.defer();
+      return $http.get('json/emails.json')
+        .success(function (data) {
+          console.log("success");
+          exports.messages = data;
+          deferred.resolve(data);
+        })
+        .error(function (data) {   console.log(data);
+          deferred.reject(data);
+        });
+      return deferred.promise;
+    };
+
+    return exports;
+  });
